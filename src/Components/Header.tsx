cca104a81270a0f62aaa2b0c9299e5f7ae73dee6
Gbrlbrgm <gabriel_bergamo@outlook.com>
2025-05-01 00:42:23 -0300

    criação dos date inputs

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useData } from "../Context/DataContext";
+import DateRange from "./DateRange";
 
 const Header = () => {
-  const { data } = useData();
-  return <div></div>;
+  return (
+    <div>
+      <DateRange />
+    </div>
+  );
 };
 
 export default Header;
